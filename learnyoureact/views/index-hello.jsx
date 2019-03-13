68b684311b1348856474a70e109193e058655548
Jeffrey McCrea <duldren@gmail.com>
2019-03-13 15:07:52 -0700

    LearnYouReact css styles as a global obj

--- learnyoureact/views/index-hello.jsx
+++ learnyoureact/views/index-hello.jsx
@@ -16,7 +16,7 @@ class TodoList extends React.Component {
   render() {
     return (
       <div className="todoList">
-        <table style={{border: "2px solid black;"}}>
+        <table style={style['table']}>
           <tbody>
             <Todo title="Shopping">Milk</Todo>
             <Todo title="Hair cut">13:00</Todo>
@@ -41,11 +41,11 @@ class Todo extends React.Component {
   render() {
     return (
     <tr>
-      <td style={{border: "1px solid black;"}}>
+      <td style={style['tableContent']}>
         <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)} />
       </td>
-      <td style={{border: "1px solid black;"}}>{this.props.title}</td>
-      <td style={{border: "1px solid black;"}}>{this.props.children}</td>
+      <td style={style['tableContent']}>{this.props.title}</td>
+      <td style={style['tableContent']}>{this.props.children}</td>
     </tr>
     );
   }
@@ -62,5 +62,14 @@ class TodoForm extends React.Component {
         I am a TodoForm.
       </div>
     )
+  }
+}
+
+let style = {
+  tableContent: {
+    border: "1px solid black;"
+  },
+  table: {
+    border: "2px solid black;"
   }
 }
